34e5f4bdb87cad55915b259b8cd4d433a0c11bf4
CHROSW <oschristophe@gmail.com>
2023-06-15 10:08:19 +0200

    Modification du css pour les images du formulaire de commande

--- wp-content/themes/planty/js/style.js
+++ wp-content/themes/planty/js/style.js
@@ -1,10 +1,10 @@
 jQuery( document ).ready(function($){
     $(document).ready(function(){
-        $('h2.wp-block-heading.has-text-align-center').css({'font-family': 'syne',});
-        $('h3.wp-block-heading.has-text-align-center').css({'font-family': 'syne',});
-        $('h3.wp-block-heading.has-text-color').css({'font-family': 'syne',});
-        $('label').css({'font-family': 'syne',});
-        $('.rnInputPrice').css({'font-family': 'syne',});
+        $('h2.wp-block-heading.has-text-align-center').css({'font-family': "'syne',sans-serif",});
+        $('h3.wp-block-heading.has-text-align-center').css({'font-family': "'syne',sans-serif",});
+        $('h3.wp-block-heading.has-text-color').css({'font-family': "'syne',sans-serif",});
+        $('label').css({'font-family': "'syne',sans-serif",});
+        $('.rnInputPrice').css({'font-family': "'syne',sans-serif",});
         $('span').css({'font-family': 'syne',});
     });
 });
