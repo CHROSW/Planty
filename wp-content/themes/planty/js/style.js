ede1a9ba278ab4908e98d0659ec941ae2a07be82
CHROSW <oschristophe@gmail.com>
2023-06-15 18:15:33 +0200

    Inversion via jquery des champs ville et code postal

--- wp-content/themes/planty/js/style.js
+++ wp-content/themes/planty/js/style.js
@@ -4,7 +4,8 @@ jQuery( document ).ready(function($){
         $('h3.wp-block-heading.has-text-align-center').css({'font-family': "'Syne', sans-serif",});
         $('h3.wp-block-heading.has-text-color').css({'font-family': "'Syne', sans-serif",});
         $('label').css({'font-family': "'Syne', sans-serif",});
-        $('.rnInputPrice').css({'font-family': "'Syne', sans-serif",});
         $('span').css({'font-family': "'Syne', sans-serif",});
+        $('.ff-name-address-wrapper.fluent-address').children().children().children().eq(1).css({'position':'relative','top':'136px',});
+        $('.ff-name-address-wrapper.fluent-address').children().children().eq(1).css({'position':'relative','bottom':'138px',});
     });
 });
