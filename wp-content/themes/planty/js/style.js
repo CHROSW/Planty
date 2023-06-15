0a2a890e230ba7085e36e378b6bfdae1d5e74d88
CHROSW <oschristophe@gmail.com>
2023-06-15 10:29:41 +0200

    Ajout de sans-serif et correction

--- wp-content/themes/planty/js/style.js
+++ wp-content/themes/planty/js/style.js
@@ -1,10 +1,10 @@
 jQuery( document ).ready(function($){
     $(document).ready(function(){
-        $('h2.wp-block-heading.has-text-align-center').css({'font-family': "'syne',sans-serif",});
-        $('h3.wp-block-heading.has-text-align-center').css({'font-family': "'syne',sans-serif",});
-        $('h3.wp-block-heading.has-text-color').css({'font-family': "'syne',sans-serif",});
-        $('label').css({'font-family': "'syne',sans-serif",});
-        $('.rnInputPrice').css({'font-family': "'syne',sans-serif",});
-        $('span').css({'font-family': 'syne',});
+        $('h2.wp-block-heading.has-text-align-center').css({'font-family': "'Syne', sans-serif",});
+        $('h3.wp-block-heading.has-text-align-center').css({'font-family': "'Syne', sans-serif",});
+        $('h3.wp-block-heading.has-text-color').css({'font-family': "'Syne', sans-serif",});
+        $('label').css({'font-family': "'Syne', sans-serif",});
+        $('.rnInputPrice').css({'font-family': "'Syne', sans-serif",});
+        $('span').css({'font-family': "'Syne', sans-serif",});
     });
 });
