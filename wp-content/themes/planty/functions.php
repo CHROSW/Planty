19982be7cd8e59ac3f245291a033f4d0797f1a16
CHROSW <oschristophe@gmail.com>
2023-06-22 13:32:22 +0200

    Indentation du fichier php

--- wp-content/themes/planty/functions.php
+++ wp-content/themes/planty/functions.php
@@ -5,12 +5,10 @@
  * (Please see https://developer.wordpress.org/themes/advanced-topics/child-themes/#how-to-create-a-child-theme)
  */
 add_action( 'wp_enqueue_scripts', 'planty_enqueue_styles' );
+
 function planty_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'child-style',
-        get_stylesheet_directory_uri() . '/style.css',
-        array('parent-style')
-    );
+    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style') );
     wp_register_script('buttonplusminusjs', get_stylesheet_directory_uri() . '/js/buttonplusminus.js', array('jquery'), '1.0.0', true);
     wp_enqueue_script('buttonplusminusjs');
     wp_register_style('font_syne_800', 'https://fonts.googleapis.com/css2?family=Syne:wght@800&display=swap', array(), null, 'all');
@@ -30,19 +28,18 @@ function planty_enqueue_styles() {
  * Your code goes below
  */
 function show_cans(){
-
     echo "<div class='cans-line'>";
     for($i=2; $i<18; $i++){
         echo "<img id='can" . $i . "' src='" . get_stylesheet_directory_uri() . "/img/planty5 ". $i . ".png' alt='can" . $i . "' />";
     }
     echo "</div>";
 }
+
 add_action('loop_end', 'show_cans');
 
 function menu_with_admin($items, $args){
-
-(is_user_logged_in())? $items.="<li><a id='extend-menu-item' class='link-added' href='" . home_url('/') . "wp-admin'>Admin</a><li>" : $items;  
-return $items;
+    (is_user_logged_in())? $items.="<li><a id='extend-menu-item' class='link-added' href='" . home_url('/') . "wp-admin'>Admin</a><li>" : $items;  
+    return $items;
 }
 
 add_filter('wp_nav_menu_items', 'menu_with_admin', 10, 2);
